ee3bf0299f1465cf5cf96b2db7121376d38b261f
David Causse <dcausse@wikimedia.org>
2020-01-21 12:48:05 +0100

    Copy updated munge script
    to support - as input
    
    Change-Id: Ic1c0bc61338ed811bf65b05a9aa2910635f1ea96

--- munge.sh
+++ munge.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 
 START=1
 END=100000
@@ -8,7 +9,7 @@ CHUNK=100000
 SKIPSITE=
 LANGS=
 
-while getopts d:f:l:s option
+while getopts d:f:l:c:s option
 do
   case "${option}"
   in
@@ -16,20 +17,21 @@ do
 	f) FROM=${OPTARG};;
 	l) LANGS=${OPTARG};;
 	s) SKIPSITE=1;;
+	c) CHUNK=${OPTARG}
   esac
 done
 
 # allow extra args
 shift $((OPTIND-1))
 
-if [ -z "$FROM" -o ! -f "$FROM" ]
+if [[ -z "$FROM" || ! ( -f "$FROM" || "$FROM" == '-' ) ]]
 then
-  echo "Usage: $0 -f <dumpfile> [-d <directory>] [-l languages]"
+  echo "Usage: $0 -f <dumpfile> [-d <directory>] [-l languages] [-c CHUNK-SIZE]"
   exit 1
 fi
 if [ -z "$LANGS" ]; then
 	ARGS=
-else 
+else
 	ARGS="--labelLanguage $LANGS --singleLabel $LANGS"
 fi
 
@@ -40,4 +42,4 @@ fi
 CP=`dirname $BASH_SOURCE`/lib/wikidata-query-tools-*-jar-with-dependencies.jar
 MAIN=org.wikidata.query.rdf.tool.Munge
 java -cp $CP $MAIN --from $FROM --to $LOCATION/$FORMAT $ARGS --chunkSize $CHUNK "$@"
-	
+
